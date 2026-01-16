5f15e920b95f4d15f68fd659253ff789f127ee6f
harika yadav <harikayadavlakshmi@gmail.com>
2026-01-16 14:23:41 +0530

    Create app.tsx

---
+++ src/services/app.tsx
@@ -0,0 +1,65 @@
+Deployment Details
+Build Failed
+Command "npm run build" exited with 1
+
+Created
+harikayadavlakshmi29-3836 Avatar
+harikayadavlakshmi29-3836
+1m ago
+Status
+Error
+Latest
+Duration
+16s
+48s ago
+Environment
+Production
+Domains
+audit-crystal-git-main-auditcrystals-projects.vercel.app
+audit-crystal-h9eo09o58-auditcrystals-projects.vercel.app
+Source
+main
+a7dd26f
+Create CNAME
+
+Deployment Settings
+3 Recommendations
+Build Logs
+16s
+19 lines
+Find in logs
+⌘F
+13:18:19.408 
+Running build in Washington, D.C., USA (East) – iad1
+13:18:19.408 
+Build machine configuration: 2 cores, 8 GB
+13:18:19.419 
+Cloning github.com/HARIKAYADAVPARIS/AUDIT-CRYSTAL- (Branch: main, Commit: a7dd26f)
+13:18:19.420 
+Skipping build cache, deployment was triggered without cache.
+13:18:19.731 
+Cloning completed: 312.000ms
+13:18:20.142 
+Running "vercel build"
+13:18:21.154 
+Vercel CLI 50.4.3
+13:18:21.790 
+Installing dependencies...
+13:18:33.931 
+npm warn deprecated node-domexception@1.0.0: Use your platform's native DOMException instead
+13:18:34.961 
+13:18:34.962 
+added 145 packages in 13s
+13:18:34.963 
+13:18:34.963 
+28 packages are looking for funding
+13:18:34.963 
+  run `npm fund` for details
+13:18:35.005 
+Running "npm run build"
+13:18:35.101 
+13:18:35.101 
+> copy-of-audit-crystal@0.0.0 build
+13:18:35.102 
+> vite build
+13:18:35.102 
